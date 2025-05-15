f1cfea9503895e4343699a93c0c393cefb01f2eb
tizianocarranza <tizianocarranza0910@gmail.com>
2025-05-15 09:36:58 -0300

    Eliminar producto

---
+++ src/lib/components/sections/eliminar.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { trash } from '$lib/icons';
+
+	import { selectedProduct } from '$lib/shared/products.svelte';
+
+	const { product } = selectedProduct;
+
+	$inspect(selectedProduct)
+</script>
+
+<section class="section eliminar">
+	<form action="?/deleteProduct" method="POST" class="section-button" title="Eliminar roducto.">
+		<input type="hidden" name="id" value={product._id} />
+		<input type="hidden" name="tipo" value={product.tipo} />
+		<button type="submit">
+			<img src={trash} alt="Trash" />
+		</button>
+	</form>
+</section>
