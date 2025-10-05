8e174dea3e9089eeb9797b99ecc76a31fc317ab0
tizianocarranza <tizianocarranza0910@gmail.com>
2025-10-05 19:18:58 -0300

    Filter by product type

---
+++ src/lib/components/buttons/filterButton.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { filter } from '$lib/icons';
+	import { slide } from 'svelte/transition';
+	import { filters } from '$lib/shared/products.svelte';
+    import { changeDisplay } from '$lib/logic/displayed';
+
+	const handleClick = () => {
+		changeDisplay('search');
+		filters.showFilters = !filters.showFilters;
+	};
+</script>
+
+<div transition:slide title="Filtrar productos." class="relative">
+	<button
+		class="hover:scale-110 transition-transform duration-300"
+		type="button"
+		onclick={handleClick}
+	>
+		<img src={filter} alt="Filter icon" class="section-button__icon" />
+	</button>
+</div>
